fbd91c0fb61a5d4e2112bfd0d52d211386a86b51
JCode918 <Willard.Joseph26@gmail.com>
2019-07-21 19:40:14 -0400

    Installlation of SQL Alchemy

---
+++ blockchain-userAuth/user.py
@@ -0,0 +1,83 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import password as p
+#from blockchain import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+
+
+class User(db.Model):
+    __tablename__='user'
+
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(80),unique=True,nullable=False)
+    password = db.Column(db.String,nullable=False)
+    authenticated = db.Column(db.Boolean,default = False)
+ 
+   
+    def __init__(self,username,password,authenticated=True):
+        self.username=username
+        self.password=p.hash_password(password)
+        self.authenticated=authenticated #add password verify
+        
+   # def newuser(self,username,password,active=True):
+    
+    def is_autenticated(self):
+        return self.authenticated
+
+    def password_verify(self): #ADD CODE TO CHECK FOR PASSWORD
+       # print("password verify")
+        user = User.query.filter_by(username=self.username).first()
+        #print("DB Password "+user.password+" Object pass "+ self.password)
+        if user.password == self.password:
+            #set authorized to true
+            return True
+        else:
+            #set authorized to false
+            return False
+
+    def exists(self):
+        #print("Username used to compare " + self.username)
+        user = User.query.filter_by(username=self.username).first()
+        #print("User inside func " + str(user))
+        #IF NONE NO USER WAS FOUND
+        if user is None: 
+            return False
+        else:
+            return True
+
+    def insert(self):
+        print("Inserting User")
+        print(self.username)
+        print(self.password)
+        print(self.authenticated)
+        db.session.add(self)
+        db.session.commit()
+
+    def print_table(self):
+        res = User.query.all()
+        for m in res:
+            print (" ID" + str(m.id) + " Username " + m.username+ " Password "+ m.password + " Authorized "+ str(m.authenticated))
+
+
+    def clear_table(self):
+        db.session.query(User).delete()
+        db.session.commit()
+
+    #def is_authenticated (self):
+        #check if password is same as stored 
+
+    #def get_id(self):
+        #return id of user
+
+
+        
+    
+    
+   
+
+
+        
