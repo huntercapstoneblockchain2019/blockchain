0e1d99955bfe192a768c62db73d074eafaa0ef9b
jamiefernandez418 <42713277+jamiefernandez418@users.noreply.github.com>
2019-07-25 16:46:25 -0400

    Update user.py

--- user.py
+++ user.py
@@ -78,41 +78,42 @@ class User(db.Model):
     #def get_id(self):
         #return id of user
         
-   	#Adds a new request to user's dictionary of requests	
-	def addBookRequest(self, isbn, rqid)
-		self.book_requests[isbn] = rqid
+   #Adds a new request to user's dictionary of requests	
+   def addBookRequest(self, isbn, rqid)
+       self.book_requests[isbn] = rqid
 	
-	def showRequests(self)
-		for key in book_requests
+   def showRequests(self)
+    '''for key in book_requests'''
+	#NOT IMPLEMENTED
 	
-	#Returns the request id of a given book 	
-	def getRequestId(self, isbn)
-		if isbn in book_requests
-			return self.book_requests[isbn]
-		else 
-			print ("No record of request for this book")
-			return None
+   #Returns the request id of a given book 	
+   def getRequestId(self, isbn)
+	if isbn in book_requests
+	   return self.book_requests[isbn]
+	else 
+	   print ("No record of request for this book")
+	   return None
 
 	
-	#Used to update user's book_ledger
-	def updateLedger(self, mledger)
-		self.book_ledger = mledger
+   #Used to update user's book_ledger
+   def updateLedger(self, mledger)
+	self.book_ledger = mledger
 	
-	#Returns the book's corresponding block chain
-	def getBookChain(self, isbn)
-		if isbn in book_ledger	
-			return book_ledger[isbn]
-		else 
-			print ("Record of book does not exist!")
-			return None
+   #Returns the book's corresponding block chain
+   def getBookChain(self, isbn)
+       if isbn in book_ledger	
+	  return book_ledger[isbn]
+       else 
+	  print ("Record of book does not exist!")
+	  return None
 	
-	#Given the request id as a parameter and the book's isbn a transaction block 
-	# is created and returned by user 
-	def createTransactionBlock(self, req_id, isbn)
-		blk_chain = self.getBookChain(isbn) # get corresponding blockchain for book with isbn
-		prev_blk = blk_chain.last_block()
-		trans_block = block.Block(prev_blk.getBlockHash(), req_id)
-		return trans_block
+   #Given the request id as a parameter and the book's isbn a transaction block 
+   # is created and returned by user 
+   def createTransactionBlock(self, req_id, isbn)
+       blk_chain = self.getBookChain(isbn) # get corresponding blockchain for book with isbn
+       prev_blk = blk_chain.last_block()
+       trans_block = block.Block(prev_blk.getBlockHash(), req_id)
+       return trans_block
 	
 
 
